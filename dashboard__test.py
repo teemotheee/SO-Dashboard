14848d271471253fb4d218de64ff65a3a2d29b6d
tjaurin <tjaurin@mpordaniel-acer>
2025-12-09 22:19:53 +0800

    Auto update: 2025-12-09 22:19:52

--- dashboard__test.py
+++ dashboard__test.py
@@ -126,7 +126,7 @@ if page == "Current Day":
         z-index: 9999;
     }
     .main-content {
-        padding-top: 70px; /* height of fixed header + some spacing */
+        padding-top: 700px; /* height of fixed header + some spacing */
     }
     </style>
     """, unsafe_allow_html=True)
